fe1a03900e0b37af695d910dc8b6d8adae9323b8
lastingyeh <lastingyeh@gmail.com>
2022-01-31 23:41:11 +0800

    update leetcode

---
+++ Leetcode/Binary Tree Preorder Traversal/index.js
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var preorderTraversal = function (root) {
+	const result = [];
+	const stack = root ? [root] : [];
+
+	while (stack.length) {
+		const curr = stack.pop();
+
+		result.push(curr.val);
+
+		if (curr.right) stack.push(curr.right);
+		if (curr.left) stack.push(curr.left);
+	}
+
+    return result;
+};
+
+// const preorderTraversal = (root) => {
+//     if (!root) return []
+// 	return [
+//          root.val,
+//          ...preorderTraversal(root.left),
+//          ...preorderTraversal(root.right)
+//     ]
+// }
---
+++ Leetcode/Single Number/index.js
@@ -0,0 +1,32 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var singleNumber = function (nums) {
+	for (let i = 0; i < nums.length; i++) {
+		if (nums.indexOf(nums[i]) === nums.lastIndexOf(nums[i])) {
+			return nums[i];
+		}
+	}
+
+    return nums[0];
+};
+
+// var singleNumber = function(nums) {
+//     nums.sort((a,b) => a - b)
+    
+//     for(let i = 0; i<nums.length;i++) {
+//         const prev = nums[i-1]
+//         const next = nums[i+1]
+//         const curr = nums[i]
+        
+//         if (prev !== curr && curr !== next){
+//             return curr;
+//         }
+//     }
+// };
+
+console.log(singleNumber([2,2,1]));
+console.log(singleNumber([4,1,2,1,2]));
+console.log(singleNumber([1]));
+
