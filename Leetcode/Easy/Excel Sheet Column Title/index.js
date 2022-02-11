fadedcd6ae678a3405381af497d14abb7637f583
Yuhung Yeh <yuhung.yeh@advantech.com.tw>
2022-02-11 18:17:35 +0800

    update leetcode

---
+++ Leetcode/Easy/Excel Sheet Column Title/index.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number} columnNumber
+ * @return {string}
+ */
+var convertToTitle = function (columnNumber) {
+    let mod = columnNumber % 26
+
+    console.log(mod);
+};
+
+convertToTitle(701)
